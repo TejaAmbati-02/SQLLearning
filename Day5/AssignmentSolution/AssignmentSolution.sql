c590d12b6f8be71312002aad36e86fb362e96699
Teja <tejaambati20@gmail.com>
2026-08-22 21:36:09 +0530

    Adding Day 5 Assignment

--- Day5/AssignmentSolution/AssignmentSolution.sql
+++ Day5/AssignmentSolution/AssignmentSolution.sql
@@ -72,25 +72,122 @@ LEFT JOIN
 ON e.dept_id = d.dep_id;
 
 
--- 4- write a query to print dep names where none of the emplyees have same salary.
+-- 4- write a query to print dep names where none of the employees have same salary.
+
+
+SELECT
+    d.dep_name
+FROM
+    namastesql.dbo.employee e
+INNER JOIN
+    namastesql.dbo.dept d
+ON
+    e.dept_id = d.dep_id
+GROUP BY
+    d.dep_name
+HAVING COUNT(d.dep_id) = COUNT(DISTINCT d.dep_id)
+
+
+
 
 
 
 -- 5- write a query to print sub categories where we have all 3 kinds of returns (others,bad quality,wrong items)
 
-
+SELECT
+    sub_category
+FROM
+    namastesql.dbo.orders AS o
+LEFT JOIN
+    namastesql.dbo.returns AS r
+ON
+    o.order_id = r.order_id
+WHERE
+    r.order_id IS NOT NULL
+GROUP BY
+    o.sub_category
+HAVING
+    COUNT(DISTINCT r.return_reason) = 3;
 
 
 
 -- 6- write a query to find cities where not even a single order was returned.
 
+SELECT
+    o.city
+FROM
+    namastesql.dbo.orders o
+LEFT JOIN
+    namastesql.dbo.returns r
+ON
+    o.order_id = r.order_id
+GROUP BY
+    o.city
+HAVING
+    COUNT(r.order_id) = 0;
+
 
 -- 7- write a query to find top 3 subcategories by sales of returned orders in east region
+
+
+
+SELECT TOP 3
+    o.sub_category, SUM(o.sales) AS sum_of_sales
+FROM
+    namastesql.dbo.orders o
+INNER JOIN
+    namastesql.dbo.returns r
+ON
+    o.order_id = r.order_id
+WHERE region = 'East'
+GROUP BY o.sub_category
+ORDER BY sum_of_sales DESC;
 
 
 -- 8- write a query to print dep name for which there is no employee
 
 
+SELECT
+    d.dep_name
+FROM
+    namastesql.dbo.employee e
+RIGHT JOIN
+    namastesql.dbo.dept d
+ON
+    e.dept_id = d.dep_id
+WHERE
+    e.emp_id IS NULL;
+
+
+SELECT d.dep_name
+FROM
+    namastesql.dbo.dept d
+LEFT JOIN
+    namastesql.dbo.employee e
+ON
+    d.dep_id = e.dept_id
+GROUP BY d.dep_name
+HAVING COUNT(e.emp_id) = 0;
+
 -- 9- write a query to print employees name for dep id is not avaiable in dept table
 
 
+
+SELECT
+    e.emp_name
+FROM
+    namastesql.dbo.employee e
+LEFT JOIN
+    namastesql.dbo.dept d
+ON
+    e.dept_id = d.dep_id
+WHERE d.dep_id IS NULL;
+
+
+
+select e.*
+FROM
+    namastesql.dbo.employee e
+LEFT JOIN
+    namastesql.dbo.dept d  on e.dept_id=d.dep_id
+where d.dep_id is null;
